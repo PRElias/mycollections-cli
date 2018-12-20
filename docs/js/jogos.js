0d3458779021fe970b9593f8221aeae420570698
Paulo Roberto Elias <ppuspfc@gmail.com>
2018-12-19 22:19:10 -0200

    fundo

--- docs/js/jogos.js
+++ docs/js/jogos.js
@@ -5,7 +5,7 @@ var app = {
 app.getGames = function () {
     var xobj = new XMLHttpRequest();
     xobj.overrideMimeType("application/json");
-    xobj.open('GET', '../json/games.json', true);
+    xobj.open('GET', '../data/games.json', true);
     xobj.onreadystatechange = function () {
         if (xobj.readyState == 4 && xobj.status == "200") {
             // .open will NOT return a value but simply returns undefined in async mode so use a callback
