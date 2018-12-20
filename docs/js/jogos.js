578469acbc8720ac3d8396bf0f08e3feed500917
Paulo Roberto Elias <ppuspfc@gmail.com>
2018-12-20 09:29:28 -0200

    alterando caminho do JSON

--- docs/js/jogos.js
+++ docs/js/jogos.js
@@ -6,7 +6,7 @@ var app = {
 app.getGames = function () {
     var xobj = new XMLHttpRequest();
     xobj.overrideMimeType("application/json");
-    xobj.open('GET', '../games/games.json', true);
+    xobj.open('GET', '../mycollections-cli/games/games.json', true);
     xobj.onreadystatechange = function () {
         if (xobj.readyState == 4 && xobj.status == "200") {
             // .open will NOT return a value but simply returns undefined in async mode so use a callback
