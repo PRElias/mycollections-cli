99a5ae78536bc994101954d7abb9b6bce1110b50
Paulo Roberto Elias <ppuspfc@gmail.com>
2018-12-20 22:17:52 -0200

    testando

--- docs/js/jogos.js
+++ docs/js/jogos.js
@@ -18,7 +18,7 @@ app.getGames = function () {
 
 app.renderizeGames = function (response) {
     app.games = JSON.parse(response);
-    app.games.sort();
+    // app.games.sort();
     var items = [];
 
     for (var index in app.games) {
