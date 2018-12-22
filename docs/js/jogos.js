1b4be778ad6c2ee817328075b0b6af1dd88dce50
Paulo Roberto Elias <ppuspfc@gmail.com>
2018-12-22 10:02:59 -0200

    novo teste ordenação

--- docs/js/jogos.js
+++ docs/js/jogos.js
@@ -20,7 +20,7 @@ app.renderizeGames = function (response) {
     app.games = JSON.parse(response);
     
     app.games.sort(function(a,b) {
-        return a.app.games.name < b.app.games.name ? -1 : a.app.games.name > b.app.games.name ? 1 : 0;
+        return a.app.games[a.index].name < b.app.games[b.index].name ? -1 : a.app.games[a.index].name > b.app.games[b.index].name ? 1 : 0;
     });
 
 
